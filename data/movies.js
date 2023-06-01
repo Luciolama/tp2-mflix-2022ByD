5a4a4bf8a8662ae2eafa3babd8d16d8f232cdb83
Luciolama <luciolattanzi1997@gmail.com>
2023-06-01 14:46:28 -0300

    terminado

--- data/movies.js
+++ data/movies.js
@@ -1,6 +1,7 @@
 const conn = require('./conn');
 const DATABASE = 'sample_mflix';
 const MOVIES = 'movies';
+const { ObjectId } = require('mongodb');
 
 
 async function getAllMovies(pageSize, page){
@@ -13,4 +14,23 @@ async function getAllMovies(pageSize, page){
     return movies;
 }
 
-module.exports = {getAllMovies};
+async function getMovieById(id){
+    const connectiondb = await conn.getConnection();
+
+    const movie = await connectiondb.db(DATABASE).collection(MOVIES).findOne({ _id: new ObjectId(id) });
+    return movie;
+}
+
+async function getMoviesWithAwards(){
+
+    const connectiondb = await conn.getConnection();
+
+    const movies = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({ 'awards.wins':{$gt:0}}, {projection : {title:1, plot:1, poster: 1, _id:false}}).toArray();
+
+    return movies;
+}
+
+module.exports = {getAllMovies, getMovieById,getMoviesWithAwards};
--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/movies');
+const moviesdb = require('../data/movies');
 
 router.get('/', async (req, res) => {    
     const pageSize = req.query.pageSize ? parseInt(req.query.pageSize): 0;
@@ -10,3 +11,17 @@ router.get('/', async (req, res) => {
 });
 
 module.exports = router;
+
+router.get('/:id', async (req,res) => {
+   
+    const moovie = await moviesdb.getMovieById(req.params.id);
+
+    res.json(moovie);
+})
+
+router.get('/awards/win', async (req,res)=>{
+
+    const movies = await moviesdb.getMoviesWithAwards();
+
+    res.json(movies);
+})
